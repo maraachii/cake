db4752ef7fcfa6921f87302dd2d5c0f0407914f8
maraachii <130571552+maraachii@users.noreply.github.com>
2023-04-15 12:42:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+alert("Happy Birthday kakakku yang cantik, sehat selalu dan banyak rezekinya ya, biar bisa jajanin adikmu ini, haha. love you!")
+let teks;
+  let day = prompt("Let's make a wish / type your wish to this");
+alert("I hope your wish come true")
+alert("Let's move to the next session, the cake!")
+let nama = prompt("Input Your Name:");
+document.write(`Hi ${nama}`);
